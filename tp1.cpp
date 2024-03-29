8c9a8b7c0cbd09eb484cfce55b67fbdfd2b9d6fc
devdias93 <belmont.programmer@gmail.com>
2024-03-29 16:38:28 -0300

    Implementa grafo básico

---
+++ tp1.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <set>
+#include <vector>
+// ¬¬ por enquanto assumimos que 'A' = 0ª letra do alfabeto (sem offsets)
+using namespace std;
+
+// O TAD do Grafo
+struct Vertex {
+  int id;
+  set<int> adj;  // lista de adjacências
+};
+
+class Graph {
+ public:
+  int size;
+  Vertex* v;
+
+  Graph(int tam) {
+    size = tam;
+    v = new Vertex[size];
+    // Atribui as ids de cada vértice
+    for (int i = 0; i < size; i++) {
+      v[i].id = i;
+    }
+  }
+
+  // Cria arestas na lista de adjacência que representa o grafo (função redundante dado que o o é um grafo não direcionado)
+  bool makeEdge(int a, int b) {
+    if ((a >= 0 && a < size) && (b >= 0 && b < size)) {  // o enunciado do TP não permite a = b
+      v[a].adj.insert(b);
+      v[b].adj.insert(a);
+      return true;
+    } else {
+      cout << "Invalid vertex in bool makeEdge(int a, int b)!" << endl;
+      cout << "(a,b,size) = (" << a << ',' << b << ',' << size << ')' << endl;
+      return false;
+    }
+  }
+
+  void print() {
+    for (int i = 0; i < size; i++) {
+      cout << '[' << v[i].id << ']';
+      for (int i : v[i].adj) {
+        cout << ' ' << i;
+      }
+      cout << endl;
+    }
+  }
+};
+
+int main() {
+  int N, M;
+
+  cin >> N >> M;
+
+  Graph G(N);
+
+  cout << "aqui 0 ";
+  for (int i = 0; i < M; i++) {
+    int a, b;  // fora do loop?? ¬
+    cin >> a >> b;
+
+    G.makeEdge(a - 1, b - 1);
+    cout << a << ' ' << b << endl;
+  }
+  cout << "aqui";
+
+  G.print();
+}
