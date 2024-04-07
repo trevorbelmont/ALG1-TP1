24c57c62392414d295e9f7b7c7044c5349be3601
devdias93 <belmont.programmer@gmail.com>
2024-04-07 16:48:09 -0300

    Adaptação de tarjan 28/30

--- opengenus/tp1TestPlusOpenGenus.cpp
+++ opengenus/tp1TestPlusOpenGenus.cpp
@@ -52,7 +52,7 @@ class Graph {
   int size;
   Vertex* v;
 
-  vector<bool> explored;
+  vector<int> explored;
   vector<int> open, min;  // open = tempo de entrada (abertura na pilha), min = min(open[v],open[vizinhos de v])
   // Em outras palavras: min de um vértice   é o mínimo entre os tempos de abertura de um vértice e seus vizinhos (recursivamente)
 
@@ -66,7 +66,7 @@ class Graph {
       v[i].parent = -1;
     }
     // Inicializa todas as variáveis auxiliares
-    explored.assign(size, false);
+    explored.assign(size, 0);
     open.assign(size, -1);
     min.assign(size, -1);
   }
@@ -109,6 +109,7 @@ class Graph {
     }
   }
 
+  // Tentativa custosa (mais que uma dfs) em tese baseada no Tarjan-Hopcroft algorithm
   // Só pode ser chamada depois da primeira DFS e retorna o component do vértice de origem, w.
   // Precisa retornar um ponteiro (?) pois, no geral, ela recebe o set<int> que retornará como entrada
   set<int>* dfsComponent(int w, int parent, bool clustered[], set<int>* component = nullptr) {
@@ -242,41 +243,60 @@ class Graph {
     // Checar se o cluster é inválido (pelo número de lows) */
     return component;
   }
-  /*
 
-  s
+  // Baseado no algoritmo de Robert Endre Tarjan como exposto em : https://iq.opengenus.org/biconnected-components/
+  set<set<int>> tarjan(set<set<int>>& allClusters, int w, set<int>& cutpoints, stack<int>& pilha, int time = -1) {
+    if (time == -1) {
+      v[w].parent = -1;
+    }
 
+    explored[w] = 1;            // Colore de explorando (1)
+    min[w] = open[w] = ++time;  // incrementa contador e depois assinala.
+    pilha.push(w);
+    int children_in_the_dfsTree = 0;  // contador de filhos na árvore da dfs (implementação do cp-algorithm)
 
-  s
+    // Olha pra vizinhança do vértice atual, w.
+    for (int u : v[w].adj) {
+      // Checa se u ainda não foi explorado ou não se está explorando
+      if (explored[u] == 0) {
+        v[u].parent = w + 1;
+        children_in_the_dfsTree++;
+        tarjan(allClusters, u, cutpoints, pilha, time);
+        min[w] = minimum(min[w], min[u]);
 
+        if (min[u] >= open[w]) {  // Encontra Candidato a cutPoint
 
-
-  */
-  int tarjan(int u) {
-    open[u] = min[u] = ++timeStamp;
-    S.push(u);
-
-    for (int i : v[u].adj) {
-      if (open[i] == 0) {
-        v[i].parent = u;
-        tarjan(i);
-        min[u] = minimum(min[u], min[i]);
-        if (min[i] >= open[u]) {
+          // Garante que é Cutpoint
+          if (v[w].parent > -1) {
+            cutpoints.insert(w);
+            v[w].isCutpoint = true;
+          }
+          set<int> cluster;
           vector<int> component;
-          int w;
+          int vertex;
+
           do {
-            w = S.top();
-            S.pop();
-            component.push_back(w);
-          } while (w != i);
-          component.push_back(u);
+            vertex = pilha.top();
+            pilha.pop();
+            component.push_back(vertex);
+            cluster.insert(vertex);
+          } while (vertex != u);
+          component.push_back(w);
+          cluster.insert(w);
+          allClusters.insert(cluster);
           components.push_back(component);
         }
-      } else if (i != v[u].parent) {
-        min[u] = minimum(min[u], open[i]);
+      } else if (u != v[w].parent) {
+        min[w] = minimum(min[w], open[u]);
       }
     }
-    return components.size();
+    if (v[w].parent == -1 && children_in_the_dfsTree >= 2) {
+      v[w].isCutpoint = true;
+      cutpoints.insert(w);
+    }
+
+    explored[w] = 2;  // Colore de já explorado (2) quando fecha o vértice
+    return allClusters;
   }
 
   int minimum(int a, int b) {
@@ -606,38 +626,34 @@ int main(int charc, char** charv) {
 
   Graph G = loadGraph();
 
-  int t = 0;
-
   // checa se a dretriz de vértice original da 1ª DFS é um vértice válido
   ROOT1STDFS = (ROOT1STDFS < G.size) ? ROOT1STDFS : 0;
 
-  set<int>* cp = G.dfs(ROOT1STDFS, -1, t);
+  set<set<int>> clusters;
+  set<int> tarjanCps;
+  int t = -1;
 
+  // Se uma raíz não trivial (lexicográfica) foi selecionada, processa ela primeiro.
+  if (ROOT1STDFS != 0) {
+    stack<int> pilhaDeComponents;  // vazia
+    clusters = G.tarjan(clusters, ROOT1STDFS, tarjanCps, pilhaDeComponents, t);
+  }
+
+  // Faz DFS nos outros vértices em ordem lexicográfica
+  for (int i = -1; i < G.size; i++) {
+    if (G.open[i] == -1) {
+      stack<int> pilhaDeComponents;  // vazia
+      clusters = G.tarjan(clusters, i, tarjanCps, pilhaDeComponents, t);
+    }
+  }
   if (VERBOSE) {
     G.print(ALFABETICAL);
     cout << endl;
-    cout << "Listing the " << (*cp).size() << " CutPoints: ";
-    printSet(*cp, ' ', ALFABETICAL, true);
+    cout << "Listing the " << tarjanCps.size() << " CutPoints: ";
+    printSet(tarjanCps, ' ', ALFABETICAL, true);
   } else {
-    cout << cp->size() << endl;
-    printSet(*cp, '\n', ALFABETICAL, false);
-  }
-
-  for (int i = 0; i < G.size; i++) {
-    G.open[i] = 0;
-  }
-  set<set<int>> clusters;  //= G.getComponents(*cp);
-  for (int i = 0; i < G.size; i++) {
-    if (G.open[i] == 0)
-      int tarjanComp = G.tarjan(i);
-  }
-
-  for (vector<int> vec : components) {
-    set<int> aux;
-    for (int i : vec) {
-      aux.insert(i);
-    }
-    clusters.insert(aux);
+    cout << tarjanCps.size() << endl;
+    printSet(tarjanCps, '\n', ALFABETICAL, false);
   }
 
   if (VERBOSE) {
@@ -663,8 +679,8 @@ int main(int charc, char** charv) {
     id++;
   }
 
-  pair<int, string> forest = ForestDump(clusters, *cp, G.size);
-  int nForest = clusters.size() + cp->size();
+  pair<int, string> forest = ForestDump(clusters, tarjanCps, G.size);
+  int nForest = clusters.size() + tarjanCps.size();
   cout << nForest << ' ' << forest.first << endl;
   cout << forest.second;
 }
